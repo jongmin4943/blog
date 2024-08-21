efffe8eef9274eeabfe34569a9ea0c8a8e57241a
Jongmin Yoon <jongmin4943@gmail.com>
2024-08-22 01:10:55 +0900

    StreamTest 추가

---
+++ BE/yjm/java-quiz/src/test/java/com/min/quiz/StreamTest.java
@@ -0,0 +1,58 @@
+package com.min.quiz;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.Stream;
+
+public class StreamTest {
+    @Test
+    void streamTest() {
+        Stream.of(1, 2, 3, 4)
+                .map(n -> {
+                    System.out.println("map");
+                    return n;
+                })
+                .filter(n -> {
+                    System.out.println("filter");
+                    return true;
+                })
+                .forEach(n -> {
+                    System.out.println("forEach");
+                });
+    }
+
+    @Test
+    void streamTestWithLimit() {
+        Stream.of(1, 2, 3, 4)
+                .map(n -> {
+                    System.out.println("map");
+                    return n;
+                })
+                .limit(2)
+                .filter(n -> {
+                    System.out.println("filter");
+                    return true;
+                })
+                .forEach(n -> {
+                    System.out.println("forEach");
+                });
+    }
+
+    @Test
+    void streamTestWithSorted() {
+        Stream.of(1, 2, 3, 4)
+                .map(n -> {
+                    System.out.println("map");
+                    return n;
+                })
+                .limit(2)
+                .filter(n -> {
+                    System.out.println("filter");
+                    return true;
+                })
+                .sorted()
+                .forEach(n -> {
+                    System.out.println("forEach");
+                });
+    }
+}
